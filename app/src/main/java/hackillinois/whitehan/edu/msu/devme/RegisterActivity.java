2554546ee323d6634a01288965686090ee1285d4
grotskyk <grotskyk@msu.edu>
2016-02-20 06:03:19 -0500

    hi

--- app/src/main/java/hackillinois/whitehan/edu/msu/devme/RegisterActivity.java
+++ app/src/main/java/hackillinois/whitehan/edu/msu/devme/RegisterActivity.java
@@ -2,9 +2,6 @@ package hackillinois.whitehan.edu.msu.devme;
 
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by kayla on 2/20/2016.
- */
 public class RegisterActivity extends AppCompatActivity {
 
 }
